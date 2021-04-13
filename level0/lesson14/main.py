670cff222be366f06913f537e7fbcf53784dbc3f
Dmitry Shakalis <shakalis@mail.ru>
2021-04-13 20:28:17 +0300

    Lesson 14

---
+++ level0/lesson14/main.py
@@ -0,0 +1,25 @@
+def Unmanned(L, N, track):
+    total_time = L
+
+    red_additional_time = 0
+
+    for i in range(N):
+        time_road = track[i][0]
+        time_red = track[i][1]
+        time_green = track[i][2]
+
+        # Total waiting time on red before the current traffic light.
+        actual_time_road = time_road + red_additional_time
+
+        # Total burning time of red and green colors.
+        time_lights_total = time_red + time_green
+
+        if time_lights_total:
+            actual_lights_moment = actual_time_road % time_lights_total
+            if actual_lights_moment < time_red:
+                wait_time = time_red - actual_lights_moment
+                red_additional_time += wait_time
+
+    total_time += red_additional_time
+
+    return total_time
---
+++ level0/lesson14/test.py
@@ -0,0 +1,26 @@
+import unittest
+import random
+
+import main
+
+
+class UnmannedTest(unittest.TestCase):
+
+    def test_reflection(self):
+        data = [
+            [10, 2, [[3, 5, 5], [5, 2, 2]], 12],
+            [8, 0, [], 8],
+            [5, 1, [[2, 0, 0]], 5],
+            [0, 0, [], 0],
+            [10, 2, [[3, 5, 5], [6, 2, 2]], 14],
+            [10, 2, [[3, 5, 5], [4, 2, 2]], 12],
+            [10, 4, [[3, 0, 2], [4, 0, 4], [6, 0, 3], [8, 0, 20]], 10],
+            [100, 10, [[5, 10, 30], [15, 5, 10], [25, 60, 20], [35, 5, 5], [45, 0, 10], [55, 15, 5], [65, 20, 10], [75, 1, 1], [85, 0, 0], [95, 5, 3]], 146],
+        ]
+
+        for i in range(len(data)):
+            dataset = data[i]
+            self.assertEqual(main.Unmanned(dataset[0], dataset[1], dataset[2]), dataset[3])
+
+if __name__ == '__main__':
+    unittest.main()
