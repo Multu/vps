958b3bce92f8aee758135b85327a13fb9e2e2521
Dmitry Shakalis <shakalis@mail.ru>
2021-05-29 14:47:31 +0300

    Lesson 27

---
+++ level0/lesson27/main.py
@@ -0,0 +1,29 @@
+def Football(f, n):
+    f_sort = list(f)
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            if f_sort[j] < f_sort[i]:
+                f_sort[j], f_sort[i] = f_sort[i], f_sort[j]
+
+    diff_positions = []
+    for i in range(n):
+        if f[i] != f_sort[i]:
+            diff_positions.append(i)
+    diff_positions_count = len(diff_positions)
+
+    # Input list was already sorted.
+    if diff_positions_count == 0:
+        return True
+    # Swap two random elements
+    elif diff_positions_count == 2:
+        return True
+    # Reverse chain of elements
+    else:
+        first_chain_index = diff_positions[0]
+        last_chain_index = diff_positions[-1]
+
+        for i in range(first_chain_index, last_chain_index + 1):
+            if f_sort[i] != f[diff_positions_count - i]:
+                return False
+        else:
+            return True
---
+++ level0/lesson27/test.py
@@ -0,0 +1,23 @@
+import unittest
+import random
+import os
+
+import main
+
+
+class FootballTest(unittest.TestCase):
+
+    def test_case_regression(self):
+        self.assertEqual(main.Football([1, 3, 2], 3), True)
+        self.assertEqual(main.Football([3, 2, 1], 3), True)
+        self.assertEqual(main.Football([1, 7, 5, 3, 9], 5), True)
+        self.assertEqual(main.Football([9, 5, 3, 7, 1], 5), False)
+        self.assertEqual(main.Football([1, 4, 3, 2, 5], 5), True)
+        self.assertEqual(main.Football([4], 1), True)
+        self.assertEqual(main.Football([2, 5], 2), True)
+        self.assertEqual(main.Football([6, 1], 2), True)
+        self.assertEqual(main.Football([1, 5, 2, 4, 3, 6, 7], 7), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
