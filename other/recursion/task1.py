a9bc04176640e3539b438e56ce1a1ec68274599f
Dmitry Shakalis <shakalis@mail.ru>
2021-06-04 15:26:58 +0300

    Recursion

--- other/recursion/task1.py
+++ other/recursion/task1.py
@@ -1,5 +1,5 @@
 def exponentiation(n, m):
     if m == 0:
         return 1
-    else:
-        return n * exponentiation(n, m - 1)
+
+    return n * exponentiation(n, m - 1)
--- other/recursion/task2.py
+++ other/recursion/task2.py
@@ -1,5 +1,5 @@
 def sum_digits(n):
     if n // 10 == 0:
         return n % 10
-    else:
-        return (n % 10) + sum_digits(n // 10)
+
+    return (n % 10) + sum_digits(n // 10)
