8e0adb64b8db51b5d20d27fa7fbcd520091a6fc0
Dmitry Shakalis <shakalis@mail.ru>
2021-06-07 16:42:24 +0300

    Recursion

--- other/recursion/task6.py
+++ other/recursion/task6.py
@@ -1,13 +1,17 @@
 def even_index_values(source):
-    if len(source) > 0:
+    if len(source) > 1:
         even_index_items = even_index_values(source[2:])
         even_index_items.insert(0, source[0])
         return even_index_items
+    elif len(source) == 1:
+        return [source[0]]
 
     return []
 
 
 def print_even_index_values(source):
-    if len(source) > 0:
+    if len(source) > 1:
         print(str(source[0]))
         print_even_index_values(source[2:])
+    elif len(source) == 1:
+        print(str(source[0]))
