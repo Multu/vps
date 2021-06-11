c8d7fcb2e198adeaeaa4747ce06a078cea33537a
Dmitry Shakalis <shakalis@mail.ru>
2021-06-11 13:22:06 +0300

    Recursion

--- other/recursion/task7.py
+++ other/recursion/task7.py
@@ -9,6 +9,13 @@ def unique(source):
     return unique_list
 
 
+def sort_desc(source):
+    for i in range(len(source) - 1):
+        for j in range(i + 1, len(source)):
+            if source[j] > source[i]:
+                source[i], source[j] = source[j], source[i]
+
+
 def second_maximum(source):
     if len(source) > 2:
         head = source[:3]
@@ -17,7 +24,7 @@ def second_maximum(source):
         # First element of `unique_head` store max value, next one - second max value.
         # The values must be different.
         unique_head = unique(head)
-        unique_head.sort(reverse=True)
+        sort_desc(unique_head)
         if len(unique_head) > 2:
             unique_head = unique_head[:2]
 
@@ -25,6 +32,6 @@ def second_maximum(source):
 
     elif len(source) == 2:
         unique_source = unique(source)
-        unique_source.sort(reverse=True)
+        sort_desc(unique_source)
         if len(unique_source) == 2:
             return unique_source[1]
