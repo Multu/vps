afa2996dd773b914aa6a3b397103638fb3ebc4ab
Multu <40861944+Multu@users.noreply.github.com>
2022-03-19 21:30:04 +0300

    Update basic_select.sql

--- hackerrank/sql/basic_select.sql
+++ hackerrank/sql/basic_select.sql
@@ -1,4 +1,5 @@
-# Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+# Query all columns for all American cities in the CITY table with populations
+# larger than 100000. The CountryCode for America is USA.
 
 SELECT 
   * 
@@ -8,7 +9,8 @@ WHERE
   AND POPULATION > 100000;
 
 
-# Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+# Query the NAME field for all American cities in the CITY table with 
+# populations larger than 120000. The CountryCode for America is USA.
 
 SELECT 
     NAME 
@@ -34,7 +36,8 @@ WHERE
     ID = 1661;
     
 
-# Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+# Query all attributes of every Japanese city in the CITY table. 
+# The COUNTRYCODE for Japan is JPN.
 
 SELECT
     *
@@ -42,7 +45,8 @@ FROM CITY
     WHERE COUNTRYCODE = 'JPN';
     
 
-# Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+# Query the names of all the Japanese cities in the CITY table. 
+# The COUNTRYCODE for Japan is JPN.
 
 SELECT
     NAME
@@ -57,7 +61,8 @@ SELECT
 FROM STATION;
 
 
-# Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+# Query a list of CITY names from STATION for cities that have an even ID number. 
+# Print the results in any order, but exclude duplicates from the answer.
 
 SELECT DISTINCT
     CITY
@@ -66,14 +71,18 @@ WHERE
     (ID % 2) = 0;
     
     
-# Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+# Find the difference between the total number of CITY entries in
+# the table and the number of distinct CITY entries in the table.
 
 SELECT
     COUNT(CITY) - COUNT(DISTINCT CITY)
 FROM STATION;
 
 
-# Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+# Query the two cities in STATION with the shortest and longest CITY names,
+# as well as their respective lengths (i.e.: number of characters in the name). 
+# If there is more than one smallest or largest city, choose the one that comes 
+# first when ordered alphabetically.
 
 SELECT 
     CITY, CHAR_LENGTH(CITY) as city_len
@@ -88,7 +97,8 @@ ORDER BY city_len DESC, CITY
 LIMIT 1;
 
 
-# Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+# Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION.
+# Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -96,7 +106,8 @@ FROM STATION
 WHERE CITY RLIKE '^[aeiou]'
 
 
-№ Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+№ Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. 
+# Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -104,7 +115,8 @@ FROM STATION
 WHERE CITY RLIKE '[aeiou]$'
 
 
-# Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+# Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u)
+# as both their first and last characters. Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -112,7 +124,8 @@ FROM STATION
 WHERE CITY RLIKE '^[aeiou].*[aeiou]$'
 
 
-# Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+# Query the list of CITY names from STATION that do not start with vowels.
+# Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -120,7 +133,8 @@ FROM STATION
 WHERE CITY NOT RLIKE '^[aeiou]'
 
 
-# Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+# Query the list of CITY names from STATION that do not end with vowels. 
+# Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -128,7 +142,8 @@ FROM STATION
 WHERE CITY NOT RLIKE '[aeiou]$'
 
 
-# Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+# Query the list of CITY names from STATION that either do not start with
+# vowels or do not end with vowels. Your result cannot contain duplicates.
 
 SELECT
     DISTINCT CITY
@@ -139,7 +154,8 @@ WHERE
     CITY NOT RLIKE '[aeiou]$'
     
     
-# Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+# Query the list of CITY names from STATION that do not start with vowels
+# and do not end with vowels. Your result cannot contain duplicates.
  
 SELECT
     DISTINCT CITY
@@ -150,7 +166,10 @@ WHERE
     CITY NOT RLIKE '[aeiou]$'
     
 
-# Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+# Query the Name of any student in STUDENTS who scored higher than  Marks.
+# Order your output by the last three characters of each name. 
+# If two or more students both have names ending in the same last 
+# three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
 
 
 SELECT
@@ -160,7 +179,8 @@ WHERE Marks > 75
 ORDER BY RIGHT(Name, 3), ID;
 
 
-# Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+# Write a query that prints a list of employee names (i.e.: the name attribute)
+# from the Employee table in alphabetical order.
 
 SELECT
     name
@@ -168,20 +188,12 @@ FROM Employee
 ORDER BY name;
 
 
-# Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than $2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+# Write a query that prints a list of employee names (i.e.: the name attribute)
+# for employees in Employee having a salary greater than $2000 per month who have 
+# been employees for less than 10 months. Sort your result by ascending employee_id.
 
 SELECT
     name
 FROM Employee
 WHERE salary > 2000 AND months < 10
 ORDER BY employee_id;
-
-
-
-
-
-
-
-
-
- 
